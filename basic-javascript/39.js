19fafb09aad54ee56de0b3bb44462285fb35cdd8
Evgeniy Chernyshenko <yevgeniyche@gmail.com>
2021-12-28 02:32:06 +0300

    Done 39 tasks from the "Basic JavaScript" section

---
+++ basic-javascript/39.js
@@ -0,0 +1,16 @@
+const myNoun = 'dog';
+const myAdjective = 'big';
+const myVerb = 'ran';
+const myAdverb = 'quickly';
+
+// Only change code below this line
+const wordBlanks =
+  'The ' +
+  myAdjective +
+  ' ' +
+  myNoun +
+  ' robbed the bank and ' +
+  myVerb +
+  ' ' +
+  myAdverb; // Change this line
+// Only change code above this line
